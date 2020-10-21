b0ad690bc042835c1b878c133a0ed5a6516caa1e
Jonathan Palacio <jpalacio0612@gmail.com>
2020-10-21 09:08:16 -0500

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  externals: ['react', 'react-dom', 'styled-components'],
+  resolve: {
+    alias: {
+      'react-neonui': path.join(__dirname, '..', 'src')
+    },
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: []
+}
