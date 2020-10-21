7a7e4bd36248f3f6673cd77acb34a54a018daa82
Jonathan Palacio <jpalacio0612@gmail.com>
2020-10-21 12:17:59 -0500

    npm published react-neon-ui@1.0.6

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,50 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// This is where we define the Inline magic.
+// This loader will turn all .svg, .jpg and .png files
+// into something that can be inlined in the final bundle
+const fileRules = {
+  test: /\.(svg|jpg|png)$/,
+  use: [
+    {
+      loader: 'url-loader',
+      options: {
+        // All files no matter what size
+        limit: Infinity
+      }
+    }
+  ]
+}
+
+// Pretty standard babel configurations for modern react apps
+const jsRules = {
+  test: /\.jsx?$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-env', '@babel/preset-react']
+    }
+  }
+}
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'index.js',
+    library: 'react-neon-ui',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    umdNamedDefine: true
   },
+
+  // Manually tell webpack about our "peerDependencies"
+  // that should not be included in the final bundle and
+  // will be provided by the Component Consumer, like an Application
   externals: ['react', 'react-dom', 'styled-components'],
-  resolve: {
-    alias: {
-      'react-neonui': path.join(__dirname, '..', 'src')
-    },
-    extensions: ['.js', '.jsx']
-  },
   module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.html$/,
-        use: {
-          loader: 'html-loader'
-        }
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: './index.html'
-    })
-  ]
+    rules: [jsRules, fileRules]
+  }
 }
